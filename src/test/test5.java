c32c72bcd9237c0386721184c4f93bf9c0d77be6
wb-chb362869 <wb-chb362869@alibaba-inc.com>
2018-04-26 14:53:34 +0800

    1

--- src/test/test5.java
+++ src/test/test5.java
@@ -1,27 +1,16 @@
 package test;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class test5 {
 
 	public static void main(String[] args) {
-		String sql =  "select * from a where ds in(${last36Month},${xxxxx}) order by ds asc";
-//		String sql =  "select * from a where ds in() order by ds asc";
-		for(int k=0;k<100;k++){
-			int i1 = sql.indexOf("${");
-			System.out.println(i1);
-			int i2 = sql.indexOf("}",i1);
-			System.out.println(i2);
-			if(i1==-1||i2==-1)
-				break;
-			String s1 = sql.substring(i1+2,i2);
-			String s2 = sql.substring(i1,i2+1);
-			System.out.println(s1);
-			System.out.println(s2);
-			sql = sql.replace(s2, "20188888");
-			System.out.println(sql);
-		}
-		
+		Long a = 1l;
+		Long b = 1l;
+		System.out.println(a==b);
+		System.out.println(a.equals(b));
 	}
 
 }
